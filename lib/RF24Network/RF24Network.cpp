e41e5aea28e55a9544244b9a1001afcc7d265bfc
Mikołaj Werner <m.werner9205@gmail.com>
2018-05-19 12:05:15 +0200

    - added receiving data from node

--- lib/RF24Network/RF24Network.cpp
+++ lib/RF24Network/RF24Network.cpp
@@ -1,6 +1,6 @@
 #include "RF24Network.h"
 
-RF24Network::RF24Network(uint16_t cePin, uint16_t csPin, String nodeAddress, char separator) : RF24(cePin, csPin)
+RF24Network::RF24Network(uint16_t cePin, uint16_t csPin, String nodeAddress, String separator) : RF24(cePin, csPin)
 {
     this->nodeAddress = nodeAddress;
     this->separator = separator;
@@ -8,12 +8,47 @@ RF24Network::RF24Network(uint16_t cePin, uint16_t csPin, String nodeAddress, cha
 
 bool RF24Network::sendMessage(String message)
 {
+    this->stopListening();
     String dataToSend = nodeAddress + String(separator) + message;
     dataToSend.toCharArray(buf, 128);
-    return this->write(&buf, strlen(buf));
+    bool status = this->write(&buf, strlen(buf));
+    this->startListening();
+    return status;
 }
 
 void RF24Network::setNodeAddress(String nodeAddress)
 {
     this->nodeAddress = nodeAddress;
+}
+
+bool RF24Network::receiveMessage(char *data)
+{
+    if (this->available())
+    {
+       
+        char msg[] = " ";
+        uint8_t len = 0;
+        len = this->getDynamicPayloadSize();
+        this->read(&msg, len);
+        char *parsed = strtok(msg, separator.c_str());
+
+        if (String(parsed).compareTo(nodeAddress) == 0)
+        {
+            char *parsedData = strtok(NULL, separator.c_str());
+            char rxBuf[128];
+            String(parsedData).toCharArray(rxBuf, 128);
+            for (uint8_t i = 0; i < String(rxBuf).length(); i++)
+            {
+                *data++ = rxBuf[i];
+            }
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    return false;
 }
--- lib/RF24Network/RF24Network.h
+++ lib/RF24Network/RF24Network.h
@@ -5,13 +5,14 @@ class RF24Network : public RF24
 
   public:
     RF24Network(uint16_t cePin, uint16_t csPin) : RF24(cePin, csPin){}
-    RF24Network(uint16_t cePin, uint16_t csPin, String nodeAddress, char separator = '|');
+    RF24Network(uint16_t cePin, uint16_t csPin, String nodeAddress, String separator = "|");
     void setNodeAddress(String nodeAddress);
     bool sendMessage(String message);
+    bool receiveMessage(char * data);
 
   private:
     String nodeAddress;
     char buf[128];
-    char separator;
+    String separator;
 
 };
